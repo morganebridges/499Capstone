a3ca73ea42d62698f96a76c1b7029960312ed9ef
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-05-25 15:01:28 -0500

    fixing test class

--- src/main/java/controllers/TransformController.java
+++ src/main/java/controllers/TransformController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransformController {
 
     @RequestMapping("/transform")
-    public String index(String input) {
-        return "\nGreetings from Spring Boot: Your input was: " + input;
+    public String index() {
+        return "Greetings from Spring Boot!";
     }
 
 }
--- src/test/java/controllers/TransformControllerTest.java
+++ src/test/java/controllers/TransformControllerTest.java
@@ -19,18 +19,18 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
-public class HelloControllerTest {
+public class TransformControllerTest {
 
     private MockMvc mvc;
 
     @Before
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+        mvc = MockMvcBuilders.standaloneSetup(new TransformController()).build();
     }
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        mvc.perform(MockMvcRequestBuilders.get("/transform").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
