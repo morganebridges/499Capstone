53019825ab4334dfab9eaf1e9c73a7a1e164d3aa
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-05-29 00:18:31 -0500

    commiting new controller stuff

--- src/main/java/com/zombie/controllers/UserController.java
+++ src/main/java/com/zombie/controllers/UserController.java
@@ -6,16 +6,28 @@ import com.zombie.models.User;
 import com.zombie.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class UserController {
 	@Autowired
 	UserRepository userRepo;
     @RequestMapping(path="/getuser", method=RequestMethod.POST)
-    public User getUser(String gamerTag) {
-        return userRepo.findByGamerTag(gamerTag);
+   
+    public ResponseEntity<User> getUser(@RequestParam(value="u") User user) {
+        User theReturn = userRepo.findByGamerTag(user.getGamerTag());
+        
+        if(theReturn != null){
+        	ResponseEntity<User> response = new ResponseEntity(theReturn, HttpStatus.OK);
+        	return response;
+        }else{
+        	return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+        
     }
 
 }
