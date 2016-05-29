3edb8633218abab1607538863224d49a15d1afb7
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-05-29 00:35:30 -0500

    fixing endpoint

--- src/main/java/com/zombie/controllers/UserController.java
+++ src/main/java/com/zombie/controllers/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zombie.models.User;
 import com.zombie.repositories.UserRepository;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +21,15 @@ public class UserController {
 	UserRepository userRepo;
     @RequestMapping(path="/getuser", method=RequestMethod.POST)
    
-    public ResponseEntity<User> getUser(@RequestParam(value="tag") String gamerTag) {
-        User theReturn = userRepo.findByGamerTag(gamerTag);
-        
-        if(theReturn != null){
-        	ResponseEntity<User> response = new ResponseEntity(theReturn, HttpStatus.OK);
-        	return response;
+    public ResponseEntity<User> getUser(@RequestParam(value="tag") String gamerTag, HttpServletRequest request, HttpServletResponse response) {
+        User user = userRepo.findByGamerTag(gamerTag);
+        ResponseEntity<User> theReturn = null;
+        if(user != null){
+        		theReturn = new ResponseEntity(user, HttpStatus.OK);
+        	return theReturn;
         }else{
         	return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        	
         }
         
     }
