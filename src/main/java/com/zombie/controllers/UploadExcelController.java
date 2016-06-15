dcce92e0f719017d902044bbb1abe37908cab86b
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-06-14 21:47:46 -0500

    single stack trace

--- src/main/java/com/zombie/controllers/UploadExcelController.java
+++ src/main/java/com/zombie/controllers/UploadExcelController.java
@@ -65,7 +65,7 @@ public class UploadExcelController {
             reader.close();
             writer.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 
