98fc6deca6a7cab79d23704d6284e4d522ccbd6a
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-06-17 20:51:23 -0500

    adding util classes

---
+++ src/main/java/com/zombie/utility/LatLng.java
@@ -0,0 +1,32 @@
+package com.zombie.utility;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class LatLng {
+	@GeneratedValue
+	@Id
+	private int id;
+	private double latitude;
+	private double longitude;
+	protected LatLng(){}
+	public LatLng(double latitude, double longitude){
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+}
