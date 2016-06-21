74011d292e117511b0d48c183a9d7e42c8e0866c
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-06-21 00:56:24 -0500

    fixes

--- src/main/java/com/zombie/controllers/UploadExcelController.java
+++ src/main/java/com/zombie/controllers/UploadExcelController.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-@RequestMapping("/uploadservice")
+@RestController
 public class UploadExcelController {
     @Autowired
     private AdminService adminService;
