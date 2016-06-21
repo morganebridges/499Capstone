bf6dc812c87f801c8381cd17944f2be9cf6ba60e
Morgan Bridges <morganebridges@Morgans-MacBook-Pro.local>
2016-06-21 00:50:26 -0500

    upload mapping update

--- src/main/java/com/zombie/controllers/UploadExcelController.java
+++ src/main/java/com/zombie/controllers/UploadExcelController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
+@RequestMapping("/uploadservice")
 public class UploadExcelController {
     @Autowired
     private AdminService adminService;
