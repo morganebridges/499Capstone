07def5a496a13461c3289d8ce9355d8d5dac5831
Corin Dennison <corin.dennison@gmil.com>
2016-07-06 00:57:02 -0500

    added logging to GCMController

--- src/main/java/com/zombie/controllers/GCMController.java
+++ src/main/java/com/zombie/controllers/GCMController.java
@@ -1,34 +1,21 @@
 package com.zombie.controllers;
 
 import com.zombie.models.User;
-import com.zombie.repositories.UserRepository;
 import com.zombie.services.NotificationService;
 import com.zombie.services.UserService;
-import com.zombie.utility.LatLng;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.zombie.models.User;
-import com.zombie.repositories.UserRepository;
-import com.zombie.utility.LatLng;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 @ComponentScan("com.fourninenine.zombie.services")
 @RequestMapping("/gcm")
 @RestController
@@ -38,33 +25,30 @@ public class GCMController {
     @Autowired
     NotificationService noteService;
 
+    private final Logger log = LoggerFactory.getLogger(GCMController.class);
+
     @RequestMapping(path="/register", method=RequestMethod.POST)
     public ResponseEntity<User>register(@RequestParam String gcmId, @RequestParam long key, HttpServletRequest request, HttpServletResponse response){
-        System.out.println("Hit GCM register endpoint");
-        System.out.println("User provided token for gcm registration : " + gcmId);
-        System.out.println("User provided key (user.id): "  + key);
+        log.trace("In GCMController.register() endpoint gcmToken={} userId={}", gcmId, key);
         User user = userService.findUserById(key);
         if(user != null){
             user.setGcmRegId(gcmId);
             userService.save(user);
 
-            System.out.println("user specs:");
-            System.out.println("clientKey: " + user.getId());
-            System.out.println("gcmId:" + gcmId);
+            log.info("Registering gcm userId={} cgmId={}", user.getId(), gcmId);
 
             user.setGcmRegId(gcmId);
             userService.save(user);
-            System.out.println("Calling gcm service");
+            log.debug("Calling gcm service");
             noteService.pushNotificationToGCM(gcmId, "Here is a message in response to your stuff that is great", user);
-            return new ResponseEntity<User>(user, HttpStatus.OK);
+            return new ResponseEntity<>(user, HttpStatus.OK);
         }else{
             if(key == -1)
-                System.out.println("Key was not found correctly on client device");
-            else System.out.println("Something strange is happening to be sure");
+                log.warn("Key was not found correctly on client device");
+            else log.error("Key={} sent but no corresponding user found in the database.", key);
+
+            return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
         }
-        //Shitty return object wont let me return null!?
-        User nullUser = null;
-        return new ResponseEntity<User>(nullUser, HttpStatus.BAD_REQUEST);
     }
 
 }
