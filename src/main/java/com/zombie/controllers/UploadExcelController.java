16d3c8f8fa55ce59d9f5b7999b3b300ebbdefbca
Corin Dennison <corin.dennison@gmil.com>
2016-07-06 00:42:58 -0500

    added logging to UploadExcelController

--- src/main/java/com/zombie/controllers/UploadExcelController.java
+++ src/main/java/com/zombie/controllers/UploadExcelController.java
@@ -3,6 +3,8 @@ package com.zombie.controllers;
 import com.zombie.models.User;
 import com.zombie.services.AdminService;
 import com.zombie.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,15 +19,19 @@ public class UploadExcelController {
 	@Autowired
 	private UserService userService;
 
+	private final Logger log = LoggerFactory.getLogger(UploadExcelController.class);
+
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String uploadFile(
             @RequestParam("uploadedFile") MultipartFile uploadedFileRef) {
 	    try {
+		    log.trace("uploading user data");
 		    long oldUserCount = userService.getUserCount();
 		    Iterable<User> oldUsers = userService.getAllUsers();
 		    int newUserCount = adminService.importUserData(uploadedFileRef);
 		    userService.deleteUsers(oldUsers);
 
+		    log.info("Successfully uploaded new user data usersRemoved={} usersAdded={}", oldUserCount, newUserCount);
 		    return newUserCount + " users were added. " + oldUserCount + " users were removed.";
 	    } catch (Exception e) {
 		    e.printStackTrace();
