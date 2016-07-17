86e76cb4b34d5b4fc9dd99f0cc8670b381935207
Morgan Bridges <morganebridges@gmail.com>
2016-07-17 03:04:16 -0500

    huh

--- src/main/java/com/zombie/repositories/ZombieRepository.java
+++ src/main/java/com/zombie/repositories/ZombieRepository.java
@@ -33,6 +33,6 @@ public interface ZombieRepository extends PagingAndSortingRepository<Zombie, Lon
 	@Query("select p from Zombie p where p.clientKey=:clientKey")
 	Stream<Zombie> streamByUserId(@Param("clientKey") long clientKey);
 
-	@Query("select p from Zombie")
+	@Query("select from Zombie")
 	List<Zombie> findAllList();
 }
