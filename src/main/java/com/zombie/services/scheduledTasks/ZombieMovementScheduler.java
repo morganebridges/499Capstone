d2667f92403c9ba9587c71ce8853443f5b48bf0f
Corin Dennison <corin.dennison@gmil.com>
2016-07-24 14:06:51 -0500

    refactored zombie movement

--- src/main/java/com/zombie/services/scheduledTasks/ZombieMovementScheduler.java
+++ src/main/java/com/zombie/services/scheduledTasks/ZombieMovementScheduler.java
@@ -1,6 +1,5 @@
 package com.zombie.services.scheduledTasks;
 
-import com.zombie.entityManagers.AbstractManager;
 import com.zombie.entityManagers.PlayerDangerManager;
 import com.zombie.models.User;
 import com.zombie.models.Zombie;
@@ -10,22 +9,22 @@ import com.zombie.services.NotificationService;
 import com.zombie.services.UserService;
 import com.zombie.services.ZombieService;
 import com.zombie.services.interfaces.communications.AlarmObserver;
-import com.zombie.utility.Geomath;
 import com.zombie.utility.Globals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Random;
 
 /**
  * Created by morganebridges on 7/16/16.
  */
 @Component
-public class ZombieMovementScheduler extends AbstractManager implements AlarmObserver {
+public class ZombieMovementScheduler implements AlarmObserver {
+    private Logger logger = LoggerFactory.getLogger(ZombieMovementScheduler.class);
+
     @Autowired
     UserService userService;
     @Autowired
@@ -35,101 +34,61 @@ public class ZombieMovementScheduler extends AbstractManager implements AlarmObs
     @Autowired
     PlayerDangerManager dangerManager;
 
-    private static HashMap<Long, Boolean> hasGenerated = new HashMap<>();
-    private static HashMap<Long, Long> lastMoved;
-    static ArrayList<User> userList = new ArrayList<User>();
     public ZombieMovementScheduler(){
-
-        super();
-        System.out.println("Zombie Movement Scheduler constructed");
+        logger.debug("Zombie Movement Scheduler constructed");
     }
+
     @Scheduled(fixedRate = Globals.ZOMBIE_MOVEMENT_REFRESH_INTERVAL)
     public void manageZombieGeneration() throws InterruptedException {
-        runWorkImpl();
+        runTask();
     }
-    protected synchronized void runWorkImpl() throws InterruptedException {
-        long lastCheck = System.currentTimeMillis();
-        System.out.println("ZombieMovementSchduler runimp");
 
-        //log.debug("ZombieGenerationManager in runWork");
-        //log.debug("Zombie HashMap runWork", userMap.entrySet());
+    protected synchronized void runTask() throws InterruptedException {
+        logger.debug("ZombieMovementScheduler runimp");
+
+        //todo register zombies or use in-memory ones instead of getting them all from the DB
         Iterable<Zombie> zombIterable = zombieService.findAll();
         zombIterable.forEach(
                         zombie ->{
-                            if(zombie.getLocation()== null)
-                                System.out.println("I'm very disappointed in you");
-                            User target = userService.findUserById(zombie.getClientKey());
-                                //System.out.println("Moving zombie: " + zombie.toString());
-
-                                //System.out.println("ZombLocation: " + zombie.getLocation().toString());
+                            if(zombie.getLocation() == null) {
+                                logger.error("Zombie found with no location");
+                                User target = userService.findUserById(zombie.getClientKey());
 
                                 LatLng newLocation = advanceTowardTarget(zombie.getLocation(), target.getLocation(), ZombieTraits.getSpeed());
-                                System.out.println("new location : " + newLocation.toString());
+                                logger.debug("Moving zombie={} location={} newLocation={}",
+                                        zombie, zombie.getLocation(), newLocation);
                                 zombie.setLongitude(newLocation.getLongitude());
                                 zombie.setLatitude(newLocation.getLatitude());
+
                                 //TODO: Need to put logic for calling an attack in here too
 
                                 zombieService.save(zombie);
-                                lastMoved.put(zombie.getId(), System.currentTimeMillis());
+                            }
                         }
                 );
-
-
-        long sleepTime = Globals.ENEMY_SPAWN_INTERVAL - (System.currentTimeMillis() - lastCheck);
-        if(sleepTime > 0){
-            Thread.sleep(Globals.ZOMBIE_MANAGER_SLEEP_INTERVAL);
-        }
     }
 
     /**
-     * A method that
+     * A method that finds a point directly in between moverLocation and targetLocation at a distance of speed
+     * from the moverLocation
      * @param moverLocation
      * @param targetLocation
      * @param speed - in meters / second
      * @return newLocation: A LatLng object that represents the new location of the mover.
      */
     private LatLng advanceTowardTarget(LatLng moverLocation, LatLng targetLocation, double speed) {
-
-        System.out.println("***** ADVANCING TOWARDS TARGET ****");
-        System.out.println(moverLocation.toString()+  " \n" + targetLocation.toString());
-        boolean moverLatHigher = (moverLocation.getLatitude() >= targetLocation.getLatitude());
-        boolean moverLongHigher = (moverLocation.getLongitude() >= targetLocation.getLongitude());
-        double newLat, newLong;
+        //TODO: SOmeday we need to fix this.  distance represented by 1 long degree gets smaller as we get closer to the poles
+        logger.debug("***** ADVANCING TOWARDS TARGET ****");
+        int latPolarity = (moverLocation.getLatitude() >= targetLocation.getLatitude()) ? -1 : 1;
+        int longPolarity = (moverLocation.getLongitude() >= targetLocation.getLongitude()) ? -1 : 1;
         Random rnd = new Random();
-        if(moverLatHigher)
-            newLat = moverLocation.getLatitude() - (speed / (rnd.nextInt(3) + .4));
-        else newLat = moverLocation.getLatitude() + (speed / (rnd.nextInt(3) + .4));
-        if(moverLongHigher)
-            newLong = moverLocation.getLongitude() - (speed / (rnd.nextInt(3) + .4));
-        else newLong = moverLocation.getLongitude() + (speed / (rnd.nextInt(3) + .4));
+
+        // This should make each unit of zombie speed = roughly 0 to 2 feet per tic
+        double newLat = moverLocation.getLatitude()
+                + (speed * (rnd.nextInt(3) + Globals.ZOMBIE_SPEED_UNIT_DISTANCE)) * latPolarity;
+        double newLong = moverLocation.getLongitude()
+                + (speed * (rnd.nextInt(3) + Globals.ZOMBIE_SPEED_UNIT_DISTANCE)) * longPolarity;
         return new LatLng(newLat, newLong);
-
-    }
-
-    public void spawnZombies(User user) {
-        //log.debug("ZombieGenerationManager.spawnZombies()");
-        System.out.println("ZombieGenerationManager.spawnZombies");
-        Globals.prln(user.toString());
-
-
-        Random rnd = new Random();
-        int numZoms = rnd.nextInt(Globals.MAXIMUM_SPAWN_NUMBER);
-        if(Math.random() <= Globals.HORDE_LIKELIHOOD_COEFFICIENT)
-            spawnHorde(user);
-
-        for(int i = 0; i < numZoms; i++){
-            Globals.prln("Number of zombies: " + numZoms);
-            //log.trace("ZombiejGenerationManager saving zombieNumber={} ", i);
-            zombieService.save(genRandomZombie(user));
-        }
-    }
-    private void spawnHorde(User user){
-        Zombie patientZero = new Zombie(user.getId(), user.getLocation());
-        Random rnd = new Random();
-        for(int i = 0; i < rnd.nextInt()%15; i++){
-            int zomNum = rnd.nextInt(Globals.MAXIMUM_SPAWN_NUMBER * 2);
-        }
-
     }
 
     @Override
@@ -141,25 +100,6 @@ public class ZombieMovementScheduler extends AbstractManager implements AlarmObs
     public void beInformed(int level) {
 
     }
-    public void requestZombiesForUser(User user, int numRequested){
-        System.out.println("Requesting zombies for: \n" + user.toString());
-        if(user.getLatitude() == 0 || user.getLongitude() == 0)
-            return;
-        for(int i = 0; i < numRequested; i++){
-
-            genRandomZombie(user);
-        }
-    }
-    public Zombie genRandomZombie(User user){
-        Globals.prln("ZombieGenerationManager.genRandomZombie");
-        Globals.prln(user.toString());
-        LatLng zomLoc = Geomath.getRandomLocationWithin(user.getLatitude(), user.getLongitude(), user.getPerceptionRange() * 3);
-        System.out.printf("ZombieGenerationManager spawnZombie %f- Zombie location random %f" , zomLoc.getLatitude(), zomLoc.getLongitude());
-        Zombie newZombie = new Zombie(user.getId(), zomLoc.getLatitude(), zomLoc.getLongitude());
-        Globals.prln("ZombieGenerationManager : zombie in spawnZombie");
-        zombieService.save(newZombie);
-        return newZombie;
-    }
 
     public boolean requestStuff(User user, int num){
         //requestList.add(new GenRequest(user, num));
@@ -169,65 +109,5 @@ public class ZombieMovementScheduler extends AbstractManager implements AlarmObs
         User user;
         int num;
         public GenRequest(User user, int num){this.user = user; this.num = num;}
-    }
-    public void registerZombie(Zombie zombie){
-
-    }
-    /* Accessors and Mutators */
-
-    public PlayerDangerManager getDangerManager() {
-        return dangerManager;
-    }
-
-    public void setDangerManager(PlayerDangerManager dangerManager) {
-        this.dangerManager = dangerManager;
-    }
-
-    public static HashMap<Long, Boolean> getHasGenerated() {
-        return hasGenerated;
-    }
-
-    public static void setHasGenerated(HashMap<Long, Boolean> hasGenerated) {
-        ZombieMovementScheduler.hasGenerated = hasGenerated;
-    }
-
-    public static HashMap<Long, Long> getLastMoved() {
-        return lastMoved;
-    }
-
-    public static void setLastMoved(HashMap<Long, Long> lastMoved) {
-        ZombieMovementScheduler.lastMoved = lastMoved;
-    }
-
-    public NotificationService getNoteService() {
-        return noteService;
-    }
-
-    public void setNoteService(NotificationService noteService) {
-        this.noteService = noteService;
-    }
-
-    public static ArrayList<User> getUserList() {
-        return userList;
-    }
-
-    public static void setUserList(ArrayList<User> userList) {
-        ZombieMovementScheduler.userList = userList;
-    }
-
-    public UserService getUserService() {
-        return userService;
-    }
-
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
-    public ZombieService getZombieService() {
-        return zombieService;
-    }
-
-    public void setZombieService(ZombieService zombieService) {
-        this.zombieService = zombieService;
     }
 }
--- src/main/java/com/zombie/utility/Globals.java
+++ src/main/java/com/zombie/utility/Globals.java
@@ -1,10 +1,7 @@
 package com.zombie.utility;
 
 
-import com.zombie.models.Zombie;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Globals {
@@ -13,6 +10,7 @@ public class Globals {
 	 * play cycle tasks. Time values are in milliseconds to be comparable to System.currentTime
 	 */
 
+	public static final double ZOMBIE_SPEED_UNIT_DISTANCE = .0000027448397;
 	public static final long ZOMBIE_LOOP_TIME = 20000;
 	//Values regarding how to deal with zombie generation
 	public static final long ENEMY_SPAWN_INTERVAL = 75000;
@@ -23,7 +21,7 @@ public class Globals {
 	public static final int WAKING_URGENCY_THRESHOLD = 7;
 
 	//Values for the movement of zombies.
-	public static final long ZOMBIE_MOVEMENT_REFRESH_INTERVAL = 20000;
+	public static final long ZOMBIE_MOVEMENT_REFRESH_INTERVAL = 10000;
 
 	//public static HashMap<Long, Zombie> tempZomList = new HashMap<>();
 	public static boolean zombiesGenerated = false;
@@ -52,6 +50,7 @@ public class Globals {
 		list.add("last used serum");
 		return list;
 	}
+	//TODO: \n is not cross-platform
 	public static void prln(String line){
 		System.out.println(line + "\n");
 	}
